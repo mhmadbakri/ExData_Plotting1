65d8ffde6f65a8ea42bbff2ad7025d8394cb4ea9
mhmadbakri <mhmadbakri@gmail.com>
2017-03-01 12:25:57 +0300

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,19 +1,15 @@
 library(data.table)
-data <- as.data.table(read.table("household_power_consumption.txt",sep = ";",header = TRUE, stringsAsFactors = FALSE, dec ="."))
+data <- read.table("household_power_consumption.txt",sep = ";",header = TRUE, stringsAsFactors = FALSE, dec =".")
 subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007"), ]
 
-subSetData <- transform(subSetData, Date = as.Date(subSetData$Date,'%d/%m/%Y'),
-                        Sub_metering_1 = as.numeric(Sub_metering_1),
-                        Sub_metering_2 = as.numeric(Sub_metering_2),
-                        Sub_metering_3 = as.numeric(Sub_metering_3))
+datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+subMetering1 <- as.numeric(subSetData$Sub_metering_1)
+subMetering2 <- as.numeric(subSetData$Sub_metering_2)
+subMetering3 <- as.numeric(subSetData$Sub_metering_3)
 
 png("plot3.png", width = 480, height = 480)
-with(subSetData, plot(Sub_metering_1, type = "n", axes = F, ylab = "Energy sub metering", xlab =""))
-with(subSetData, lines(Sub_metering_1,col="black"))
-with(subSetData, lines(Sub_metering_2,col="red"))
-with(subSetData, lines(Sub_metering_3,col="blue"))
-axis(side=1, at=c(0,1441, 2880),labels= c("Thu","Fri","Sat"))
-axis(side=2)
-box()
-legend("topright" , lwd = 2, col=c("black","red","blue"), legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+plot(datetime, subMetering1, type="l", xlab="", ylab="Energy sub metering")
+lines(datetime, subMetering2, type="l", col="red")
+lines(datetime, subMetering3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
 dev.off()
